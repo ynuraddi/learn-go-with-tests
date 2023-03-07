c9ebdafe316fac82df020a4fd0bc9cf9f9d2f363
ynuraddi <definston@gmail.com>
2023-03-07 21:25:32 +0600

    20%

---
+++ helloworld/hello.go
@@ -0,0 +1,26 @@
+package helloworld
+
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Hello, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	}
+
+	return prefix + name
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	a, b := map[int]int{}, make(map[int]int)
+
+	fmt.Println(a, b)
+}
