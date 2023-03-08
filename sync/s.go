8f1fe5fe06331ff476d767e8e99977492e92fe13
ynuraddi <definston@gmail.com>
2023-03-09 05:46:36 +0600

    sync

---
+++ sync/s.go
@@ -0,0 +1,18 @@
+package sync
+
+import "sync"
+
+type Counter struct {
+	sync.Mutex
+	value int
+}
+
+func (c *Counter) Inc() {
+	c.Lock()
+	defer c.Unlock()
+	c.value++
+}
+
+func (c *Counter) Value() int {
+	return c.value
+}
